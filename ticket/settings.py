896399e9b2b4f9d6bba88ced9f66df47c792cccb
imaxct <imaxct@foxmail.com>
2017-11-27 17:01:57 +0800

    关闭DevMode.

--- ticket/settings.py
+++ ticket/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@!+k&3f4=$iub^_ue%lii7jd(!)7_@1gy%yip129ry77%d25ry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
